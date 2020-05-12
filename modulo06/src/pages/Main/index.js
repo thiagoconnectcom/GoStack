4f6efa3110290ca2b2c775bb9c233dea5e807aa4
Thiago Taveiros <thiago.taveiros@btbtelecom.local>
2020-05-12 18:56:36 -0300

    commit initial react native

---
+++ modulo06/src/pages/Main/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {Container} from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <Text>Asfasf</Text>
+    </Container>
+  );
+}
+
+Main.navigationOptions = {
+  title: 'Usuários',
+};
